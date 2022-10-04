457f5dda7ba05d836dcd2bc9c656e4347f6f13d1
Leona Juan <leonajuan@gmail.com>
2022-10-04 17:28:07 -0400

    added login form and routers

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import { useEffect } from "react";
 import './App.css';
 import Header from './components/Header'
@@ -14,10 +20,42 @@ function App() {
   }, [])
 
   return (
-    <div>
-      <Header />
+    <>
+      <Router>
+        <Header />
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Locations</Link>
+              </li>
+              <li>
+                <Link to="/about">Reviews</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/locations">
+              {/* <About /> */}
+            </Route>
+            <Route path="/reviews">
+              {/* <Users /> */}
+            </Route>
+            <Route path="/users">
+              {/* <Home /> */}
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+
       <NewReviewForm />
-    </div>
+    </>
   );
 }
 
---
+++ src/components/LoginForm.js
@@ -0,0 +1,7 @@
+function LoginForm() {
+  return (
+    null
+  )
+}
+
+export default LoginForm;
