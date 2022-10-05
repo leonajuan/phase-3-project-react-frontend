b5a7972aba357b0310dfef6f383c9a0d1ae51cfa
Leona Juan <leonajuan@gmail.com>
2022-10-04 20:28:27 -0400

    began working with users data for login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,28 @@
+import { useEffect } from "react";
+
 function LoginForm() {
+
+  useEffect(() => {
+    fetch("http://localhost:9292/users")
+      .then(res => res.json())
+      .then(usersData => {
+        usersData.map(user => {
+          console.log(user)
+        })
+      })
+  }, [])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
+
   return (
     <div className="login-form">
       <h2>Log In</h2>
       <form>
         <input type="text" name="name" label="Username" placeholder="Username" />
         <input type="text" name="name" label="Password" placeholder="Password" />
-        <button>Submit</button>
+        <button onSubmit={handleSubmit}>Submit</button>
       </form>
     </div>
   )
